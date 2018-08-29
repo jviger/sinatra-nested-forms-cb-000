0c98c94acf33ace5633c262fcb3188123d25cf6b
Jason Viger <jason.viger@gmail.com>
2018-08-29 05:57:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    
+    post "/pirates" do 
+      @pirate = Pirate.new(params[:name], params[:height], params[:weight])
+      
+      erb :'pirates/show'
+    end
+    
+    get "/" do 
+      
+      erb :'pirates/new'
+    end
 
-  end
+    end
 end
